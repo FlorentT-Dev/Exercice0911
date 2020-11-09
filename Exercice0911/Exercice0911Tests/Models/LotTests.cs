116141bae1565c5b5e2934a842bc8d6448f84adc
FlorentT-Dev <59725973+FlorentT-Dev@users.noreply.github.com>
2020-11-09 14:16:05 +0100

    test unitaire

---
+++ Exercice0911/Exercice0911Tests/Models/LotTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercice0911.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice0911.Models.Tests
+{
+    [TestClass()]
+    public class LotTests
+    {
+        [TestMethod()]
+        public void AjouterTachesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DonneNbTachesTermineesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PourcentageAvancementTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DonneTacheAAffecterTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Exercice0911/Exercice0911Tests/Models/ProjetTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercice0911.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice0911.Models.Tests
+{
+    [TestClass()]
+    public class ProjetTests
+    {
+        [TestMethod()]
+        public void pourcentageAvancementTest()
+        {
+            Projet p1 = new Projet(1,"Création",02/12/2020,02/14/2020,05/14/2020,06/02/2020,"Fini",);
+            float result = p1.pourcentageAvancement();
+            Assert.AreEqual(result, false);
+
+        }
+    }
+}
