c1484f22443f5d3ff7509fb77dd604527b2d2cd3
FlorentT-Dev <59725973+FlorentT-Dev@users.noreply.github.com>
2020-11-09 09:29:32 +0100

    Collaborateur et Service

--- Exercice0911/Exercice0911/Models/Collaborateur.cs
+++ Exercice0911/Exercice0911/Models/Collaborateur.cs
@@ -8,5 +8,31 @@ namespace Exercice0911.Models
 {
     class Collaborateur
     {
+        #region Attributs
+        public static List<Collaborateur> collClassCollaborateur = new List<Collaborateur>();
+        private int _idCollaborateur;
+        private string _nomCollaborateur;
+        private Service _leService;
+        #endregion
+
+        #region Constructeur
+        public Collaborateur(int idCollaborateur, string nomCollaborateur, Service leService)
+        {
+            _idCollaborateur = idCollaborateur;
+            _nomCollaborateur = nomCollaborateur;
+            _leService = leService;
+            collClassCollaborateur.Add(this);
+        }
+        #endregion
+
+        #region Getters-Setters
+        public int IdCollaborateur { get => _idCollaborateur; set => _idCollaborateur = value; }
+        public string NomCollaborateur { get => _nomCollaborateur; set => _nomCollaborateur = value; }
+        public Service LeService { get => _leService; set => _leService = value; }
+        #endregion
+
+        #region Méthodes
+        #endregion
+
     }
 }
--- Exercice0911/Exercice0911/Models/Service.cs
+++ Exercice0911/Exercice0911/Models/Service.cs
@@ -8,5 +8,31 @@ namespace Exercice0911.Models
 {
     class Service
     {
+        #region Attributs
+        public static List<Service> collClassService = new List<Service>();
+        private int _idService;
+        private string _nomService;
+        private Collaborateur _leResponsable;
+        #endregion
+
+        #region Constructeur
+        public Service(int idService, string nomService, Collaborateur unResponsable)
+        {
+            _idService = idService;
+            _nomService = nomService;
+            _leResponsable = unResponsable;
+            collClassService.Add(this);
+
+        }
+        #endregion
+
+        #region Getters-Setters
+        public int IdService { get => _idService; set => _idService = value; }
+        public string NomService { get => _nomService; set => _nomService = value; }
+        public Collaborateur LeResponsable { get => _leResponsable; set => _leResponsable = value; }
+        #endregion
+
+        #region Méthodes
+        #endregion
     }
 }
